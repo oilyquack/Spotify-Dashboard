a383d3be80cd41f17667e73b5f3e741359ee5719
Ollie Crook <olliejcrook@gmail.com>
2018-08-22 11:47:16 +0100

    add snapshot test for shallow render

--- tests/App.test.js
+++ tests/App.test.js
@@ -3,7 +3,7 @@ import App from "../src/components/App";
 import renderer from "react-test-renderer";
 
 describe("App", () => {
-  it(",atches the snapshot", () => {
+  it("matches the snapshot", () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
